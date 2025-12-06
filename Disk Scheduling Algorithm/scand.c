634ba82ce91622ca705fc860f5c273eb99135563
lakhdive-rmc <lakhdive@gmail.com>
2025-12-06 15:15:06 +0530

    SCAN disk scheduling algorithm with Head Direction
    Implement SCAN disk scheduling algorithm to service requests based on head movement direction.

---
+++ Disk Scheduling Algorithm/scand.c
@@ -0,0 +1,102 @@
+/Also callled as Elevator Algorithm, Disk arm moves in one direction, servicing requests until it reaches the end, then reverses.
+//It provides better performance by reducing overall seek time. It also accept direction of head movement.
+#include<stdio.h>
+#include<stdlib.h>
+void sort(int req[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; ++i) {
+        for (j = i + 1; j < n; ++j) {
+            if (req[i] > req[j]) {
+                temp = req[i];
+                req[i] = req[j];
+                req[j] = temp;
+            }
+        }
+    }
+}
+void scan(int req[], int n, int head,int maxc,int  d)
+{
+	int i, hm,index,total=0;
+	// Find the index of the first request greater than the current head position
+
+	for (i = 0; i < n; ++i) {
+            if (req[i] > head) {
+                index = i;
+                break;
+            }
+        }
+
+
+	if (d == 2)
+	{
+	printf("Direction: Right\n");
+
+        // Serve requests from the beginning of the disk to the end
+        for (i = index; i < n; i++) {
+            hm = abs(req[i] - head);
+            printf("Request %d served. Head movement: %d\n", req[i], hm,total);
+            total = total + hm;
+            head = req[i];
+        	}
+	hm = abs(maxc - head -1);
+	total = total + hm;
+	head=maxc-1;
+	printf("Reached to Right End, Head Movement : %d\n",hm);
+	// Serve requests in reverse order until the beginning of the disk
+        for (i = index - 1; i >= 0; i--) {
+    		hm = abs(req[i] - head);
+            	printf("Request %d served. Head movement: %d\n", req[i], hm);
+            	total = total + hm;
+            	head = req[i];
+        	}
+	}
+	else if (d == 1)
+	{
+	    printf("Direction: Left\n");
+
+            for (i = index - 1; i >= 0; i--) {
+            	hm = abs(req[i] - head);
+            	printf("Request %d served. Head movement: %d\n", req[i], hm);
+            	total = total + hm;   
+            	head = req[i];
+	        }
+	hm = abs(0 - head);
+        total = total + hm;
+        head=0;
+        printf("Reached to Left End, Head Movement : %d\n",hm);
+	for (i = index; i < n; i++) {
+            hm = abs(req[i] - head);
+            printf("Request %d served. Head movement: %d\n", req[i], hm,total);
+            total = total + hm;
+            head = req[i];
+                }
+	}
+
+	printf("\nTotal Head Movement=%d",total);
+}
+void main()
+{
+	int req[20],i,n,head,maxc;
+	int direction;
+	printf("Enter the no.of processes to be requested : ");
+	scanf("%d",&n);
+	printf("Enter the request : ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&req[i]);
+	}
+	// Sort the Requests in ascending order
+	sort(req,n);
+	printf("\nSorted Request : ");
+	for(i=0; i<n;i++)
+		printf("\t%d",req[i]);
+	printf("Enter the initial head position : ");
+	scanf("%d",&head);
+	printf("\nEnter Number of Cylinder : ");
+	scanf("%d",&maxc);
+	printf("\nEnter Direction of Head (1 for Left / 2 for Right) : ");
+	scanf("%d",&direction);
+	printf("\nSeek Sequence : %d",head);
+
+	scan(req,n,head,maxc,direction);
+}
