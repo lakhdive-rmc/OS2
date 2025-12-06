8a0ccd22f2342b8aa58d850c47c091785d844b03
lakhdive-rmc <lakhdive@gmail.com>
2025-12-06 15:19:40 +0530

    LOOK disk scheduling algorithm implementation
    Implement LOOK disk scheduling algorithm to serve requests in a specific order based on the current head position.

---
+++ Disk Scheduling Algorithm/look.c
@@ -0,0 +1,71 @@
+//This algorithm is sSimilar to SCAN but reverses direction at the last request instead of going to the disk end.
+
+#include<stdio.h>
+#include<stdlib.h>
+void sort(int req[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; ++i) {
+        for (j = i + 1; j < n; ++j) {
+            if (req[i] > req[j]) {
+                temp = req[i];
+                req[i] = req[j];
+                req[j] = temp;
+            }
+        }
+    }
+}
+void look(int req[], int n, int head)
+{
+	int i, hm,index,total=0;
+	printf("Direction: Right\n");
+        // Find the index of the first request greater than the current head position
+        for (i = 0; i < n; ++i) {
+            if (req[i] > head) {
+                index = i;
+                break;
+            }
+        }
+        // Serve requests from the beginning of the disk to the end
+        for (i = index; i < n; i++) {
+            hm = abs(req[i] - head);
+            printf("Request %d served. Head movement: %d\n", req[i], hm,total);
+            total = total + hm;
+            head = req[i];
+        }
+//	hm = abs(maxc - head);
+//	total = total + hm;
+//	head=maxc;
+	printf("Reached to Right End Last Request Served\n");
+// Serve requests in reverse order until the beginning of the disk
+        for (i = index - 1; i >= 0; i--) {
+	    hm = abs(req[i] - head);
+            printf("Request %d served. Head movement: %d\n", req[i], hm);
+            total = total + hm;
+            head = req[i];
+
+        }
+	printf("\nTotal Head Movement=%d",total);
+}
+void main()
+{
+	int req[20],i,n,head;
+	printf("Enter the no.of processes to be requested : ");
+	scanf("%d",&n);
+	printf("Enter the request : ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&req[i]);
+	}
+	// Sort the Requests in ascending order
+	sort(req,n);
+	printf("\nSorted Request : ");
+	for(i=0; i<n;i++)
+		printf("\t%d",req[i]);
+	printf("Enter the initial head position : ");
+	scanf("%d",&head);
+//	printf("\nEnter Max Cylinder Number : ");
+//	scanf("%d",&maxc);
+	printf("\nSeek Sequence : %d",head);
+
+	look(req,n,head);
+}
